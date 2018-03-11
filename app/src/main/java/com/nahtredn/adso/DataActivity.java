48ac37eea13e7cc77759167813119f7dac001c1b
Alexander Padilla <yamop.16@gmail.com>
2018-03-10 19:11:25 -0600

    Added data activity functionality

--- app/src/main/java/com/nahtredn/adso/DataActivity.java
+++ app/src/main/java/com/nahtredn/adso/DataActivity.java
@@ -7,23 +7,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class DataActivity extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null){
+            toolbar.setTitle("Mis datos");
+            toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back));
+            setSupportActionBar(toolbar);
+        }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+        mAdView = findViewById(R.id.adViewData);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 }
