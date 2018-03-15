37497ecd63110a925891576672f161ec2c1d0e15
Alexander Padilla <yamop.16@gmail.com>
2018-03-14 21:42:04 -0600

    Functionality added

--- app/src/main/java/com/nahtredn/adso/PhotoActivity.java
+++ app/src/main/java/com/nahtredn/adso/PhotoActivity.java
@@ -14,10 +14,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ContextThemeWrapper;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -26,17 +28,18 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 public class PhotoActivity extends AppCompatActivity {
-    private Button btnTakePhoto;
-    private Button btnSelectPhoto;
+    private static final String photoPreferences = "photoPreferences";
+    private static final String photoProperty = "photo";
+    private static final String photoFileName = "profile_photo";
+    private static final String photoFileExtension = "jpg";
+    private static final String dirChild = "content";
+
     private InterstitialAd mInterstitialAd;
     private ImageView photo;
 
     private static final int TAKE_PHOTO_REQUEST_CODE = 1;
     private static final int SELECT_PHOTO_REQUEST_CODE = 2;
     private static final int CROP_PHOTO_REQUEST_CODE = 3;
-    private boolean isCameraLaunched;
-
-    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,27 +52,7 @@ public class PhotoActivity extends AppCompatActivity {
             actionBar.setDisplayShowHomeEnabled(true);
         }
 
-        btnTakePhoto = findViewById(R.id.btn_take_picture_photo);
-        btnTakePhoto.setEnabled(false);
-        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isCameraLaunched = true;
-                showInterstitial();
-            }
-        });
-
-        btnSelectPhoto = findViewById(R.id.btn_select_picture_photo);
-        btnSelectPhoto.setEnabled(false);
-        btnSelectPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isCameraLaunched = false;
-                showInterstitial();
-            }
-        });
-
-        mAdView = findViewById(R.id.adViewPhoto);
+        AdView mAdView = findViewById(R.id.adViewPhoto);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
@@ -81,10 +64,7 @@ public class PhotoActivity extends AppCompatActivity {
     }
 
     private void loadImage(){
-        SharedPreferences prefs =
-                getSharedPreferences("ProfilePreferences",Context.MODE_PRIVATE);
-
-        String pathPhoto = prefs.getString("photo", null);
+        String pathPhoto = getImageUrl();
         if (pathPhoto != null){
             File imgFile = new  File(pathPhoto);
             if(imgFile.exists()){
@@ -97,6 +77,12 @@ public class PhotoActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.common, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
@@ -105,7 +91,35 @@ public class PhotoActivity extends AppCompatActivity {
             return true;
         }
 
+        if (id == R.id.action_delete){
+            saveImageUrl(null);
+            photo.setImageDrawable(getDrawable(R.drawable.ic_profile));
+            showMessage(getString(R.string.photo_profile_deleted_activity_photo));
+            return true;
+        }
+
+        if (id == R.id.action_help){
+            AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.commonAlertDialog));
+            builder.setMessage(getString(R.string.help_photo_activity))
+                    .setTitle(getString(R.string.help_title));
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClicButtonPhoto(View view){
+        showInterstitial();
+        if (view.getId() == R.id.btn_take_picture_photo){
+            launchActivityForUpdatePictureProfile(true);
+            return;
+        }
+        if (view.getId() == R.id.btn_select_picture_photo){
+            launchActivityForUpdatePictureProfile(false);
+            return;
+        }
     }
 
     private InterstitialAd newInterstitialAd() {
@@ -114,21 +128,15 @@ public class PhotoActivity extends AppCompatActivity {
         interstitialAd.setAdListener(new AdListener() {
             @Override
             public void onAdLoaded() {
-                btnTakePhoto.setEnabled(true);
-                btnSelectPhoto.setEnabled(true);
             }
 
             @Override
             public void onAdFailedToLoad(int errorCode) {
-                btnTakePhoto.setEnabled(true);
-                btnSelectPhoto.setEnabled(true);
-                launchActivityForUpdatePictureProfile();
             }
 
             @Override
             public void onAdClosed() {
-                goToNextLevel();
-                launchActivityForUpdatePictureProfile();
+                reloadInterstitial();
             }
         });
         return interstitialAd;
@@ -138,25 +146,23 @@ public class PhotoActivity extends AppCompatActivity {
         if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
             mInterstitialAd.show();
         } else {
-            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
-            goToNextLevel();
+            reloadInterstitial();
         }
     }
 
     private void loadInterstitial() {
-        btnTakePhoto.setEnabled(false);
         AdRequest adRequest = new AdRequest.Builder()
                 .setRequestAgent("android_studio:ad_template").build();
         mInterstitialAd.loadAd(adRequest);
     }
 
-    private void goToNextLevel() {
+    private void reloadInterstitial() {
         mInterstitialAd = newInterstitialAd();
         loadInterstitial();
     }
 
-    private void launchActivityForUpdatePictureProfile() {
-        if (isCameraLaunched) {
+    private void launchActivityForUpdatePictureProfile(boolean takePicture) {
+        if (takePicture) {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                 startActivityForResult(takePictureIntent, TAKE_PHOTO_REQUEST_CODE);
@@ -185,7 +191,14 @@ public class PhotoActivity extends AppCompatActivity {
                 startCrop(data.getData());
                 break;
             case (CROP_PHOTO_REQUEST_CODE):
+                if (resultCode != RESULT_OK){
+                    return;
+                }
                 Bundle extras = data.getExtras();
+                if (extras == null){
+                    showMessage(getString(R.string.error_croped_photo));
+                    return;
+                }
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
                 photo.setImageBitmap(imageBitmap);
                 String fileName = saveImageFile(imageBitmap);
@@ -210,7 +223,11 @@ public class PhotoActivity extends AppCompatActivity {
         intent.putExtra("scale", "true");
         intent.putExtra("return-data", true);
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-        startActivityForResult(intent, CROP_PHOTO_REQUEST_CODE);
+        try {
+            startActivityForResult(intent, CROP_PHOTO_REQUEST_CODE);
+        } catch (SecurityException s){
+            showMessage(getString(R.string.error_selected_photo));
+        }
     }
 
     public String saveImageFile(Bitmap bitmap) {
@@ -227,19 +244,25 @@ public class PhotoActivity extends AppCompatActivity {
 
     private String getFilename() {
         File file = new File(getFilesDir()
-                .getPath(), "content");
+                .getPath(), dirChild);
         file.mkdirs();
-        String uriSting = (file.getAbsolutePath() + "/"
-                + "photo_profile.jpg");
-        return uriSting;
+        return  (file.getAbsolutePath() + "/" + photoFileName + "." + photoFileExtension);
     }
 
     private void saveImageUrl(String pathPhoto){
-        SharedPreferences prefs =
-                getSharedPreferences("ProfilePreferences", Context.MODE_PRIVATE);
-
+        SharedPreferences prefs = getSharedPreferences(photoPreferences, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("photo", pathPhoto);
+        editor.putString(photoProperty, pathPhoto);
         editor.apply();
+        editor.clear();
+    }
+
+    private String getImageUrl(){
+        SharedPreferences prefs = getSharedPreferences(photoPreferences,Context.MODE_PRIVATE);
+        return prefs.getString(photoProperty, null);
+    }
+
+    private void showMessage(String message){
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
     }
 }
