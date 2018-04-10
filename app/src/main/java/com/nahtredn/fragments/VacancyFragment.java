58dc77e7fde77dbe47df3f443530ae2dfe0cc695
Alexander Padilla <yamop.16@gmail.com>
2018-04-09 21:04:26 -0500

    Conexión a Tyk agregada

--- app/src/main/java/com/nahtredn/fragments/VacancyFragment.java
+++ app/src/main/java/com/nahtredn/fragments/VacancyFragment.java
@@ -1,19 +1,48 @@
 package com.nahtredn.fragments;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.internal.gmsg.HttpClient;
 import com.nahtredn.adso.R;
 import com.nahtredn.entities.Address;
 import com.nahtredn.entities.Vacancy;
 import com.nahtredn.adapters.VacancyAdapter;
 import com.nahtredn.utilities.Messenger;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +53,9 @@ import java.util.List;
 public class VacancyFragment extends Fragment {
     private ListView mVacancyList;
     private VacancyAdapter mVacancyAdapter;
+    private ProgressDialog pDialog;
+    private String result;
+    private ArrayList<Vacancy> vacancies;
 
     public VacancyFragment() {
         // Required empty public constructor
@@ -38,33 +70,17 @@ public class VacancyFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            // Gets parámetros
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_vacancy, container, false);
+        mVacancyList = root.findViewById(R.id.vacancy_list);
 
-        // Instancia del ListView.
-        mVacancyList = (ListView) root.findViewById(R.id.vacancy_list);
+        vacancies = new ArrayList<>();
 
-        // Inicializar el adaptador con la fuente de datos.
-        mVacancyAdapter = new VacancyAdapter(getActivity(),
-                getVacancies());
-
-        //Relacionando la lista con el adaptador
-        mVacancyList.setAdapter(mVacancyAdapter);
-
-        // Eventos
-        mVacancyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Messenger.with(getActivity()).showMessage("Vacancy pressed");
-            }
-        });
+        loadData();
 
         setHasOptionsMenu(true);
         return root;
@@ -74,11 +90,11 @@ public class VacancyFragment extends Fragment {
     public void onResume() {
         super.onResume();
         mVacancyAdapter = new VacancyAdapter(getActivity(),
-                getVacancies());
+                vacancies);
         mVacancyList.setAdapter(mVacancyAdapter);
     }
 
-    private List<Vacancy> getVacancies(){
+    /*private List<Vacancy> getVacancies(){
         ArrayList<Vacancy> vacancies = new ArrayList<>();
         Vacancy vacancy = new Vacancy("Gerente","FEMSA","$$");
         Address address = new Address("Guadalupe","Zacatecas");
@@ -93,5 +109,115 @@ public class VacancyFragment extends Fragment {
         vacancies.add(v2);
         vacancies.add(v2);
         return vacancies;
+    }*/
+
+    public void loadData(){
+        new FetchVacants().execute("https://naht-redn-dev.cloud.tyk.io/vacancies/availables");
+        // new FetchVacants().execute("");
+    }
+
+    class FetchVacants extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            System.out.println("Starting download");
+
+            pDialog = new ProgressDialog(getActivity());
+            pDialog.setMessage("Loading... Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * Downloading file in background thread
+         * */
+        @Override
+        protected String doInBackground(String... f_url) {
+            try {
+                System.out.println("Downloading");
+                URL url = new URL(f_url[0]);
+
+                URLConnection conection = url.openConnection();
+                conection.setConnectTimeout(5000);
+                conection.connect();
+
+                InputStream input = new BufferedInputStream(url.openStream());
+
+                try {
+                    BufferedReader bReader = new BufferedReader(new InputStreamReader(input, "utf-8"), 8);
+                    StringBuilder sBuilder = new StringBuilder();
+
+                    String line = null;
+                    while ((line = bReader.readLine()) != null) {
+                        sBuilder.append(line + "\n");
+                    }
+
+                    input.close();
+                    result = sBuilder.toString();
+
+                } catch (Exception e) {
+                    Log.e("VacancyFragment", "Error converting result " + e.toString());
+                }
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task
+         * **/
+        @Override
+        protected void onPostExecute(String file_url) {
+
+            //parse JSON data
+            try {
+                JSONArray jArray = new JSONArray(result);
+                for(int i=0; i < jArray.length(); i++) {
+
+                    JSONObject jObject = jArray.getJSONObject(i);
+                    Vacancy vacancy = new Vacancy(jObject.getString("job_title"),
+                            jObject.getString("company"),
+                            jObject.getString("salary"));
+                    vacancy.setSalary(jObject.getString("salary"));
+                    Address address = new Address(jObject.getString("municipality"), jObject.getString("state"));
+                    vacancy.setAddress(address);
+                    vacancies.add(vacancy);
+                    Log.w("VacancyFragment", "vacancy fetched: " + jObject.getString("job_title"));
+
+                } // End Loop
+            } catch (JSONException e) {
+                Messenger.with(getActivity()).showMessage("Error al consultar las vacantes en el servidor");
+                Log.e("JSONException", "Error: " + e.toString());
+            } catch (NullPointerException npe) {
+                Messenger.with(getActivity()).showMessage("No se encontró ningúna vacante");
+                Log.e("NullPointerException", "Error: " + npe.toString());
+            }
+
+            // System.out.println("Downloaded: " + file_url);
+            pDialog.dismiss();
+
+            // Inicializar el adaptador con la fuente de datos.
+            mVacancyAdapter = new VacancyAdapter(getActivity(),
+                    vacancies);
+
+            //Relacionando la lista con el adaptador
+            mVacancyList.setAdapter(mVacancyAdapter);
+
+            // Eventos
+            mVacancyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                    Messenger.with(getActivity()).showMessage("Vacancy pressed");
+                }
+            });
+        }
+
     }
 }
